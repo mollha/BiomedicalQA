eb80e1252d1cf1b2a8eb8f8b0942d4c4bc97d3b5
moll_ha <molly.hayward@durham.ac.uk>
2020-11-09 22:24:46 +0000

    Added more electra

--- Code/ELECTRA.py
+++ Code/ELECTRA.py
@@ -4,9 +4,29 @@ import torch
 tokenizer = ElectraTokenizer.from_pretrained('google/electra-small-discriminator')
 model = ElectraModel.from_pretrained('google/electra-small-discriminator')
 
-input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
-outputs = model(input_ids)
+# input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
+# outputs = model(input_ids)
+#
+# last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
+#
+# print(last_hidden_states)
 
-last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
+question_text = "This is my question"
+answer_text = "This is my answer"
 
-print(last_hidden_states)
+# input_ids = tokenizer.encode("Hello, my dog is cute", add_special_tokens=True)
+input_ids = tokenizer.encode(question_text, answer_text, add_special_tokens=True)
+
+print('The input has a total of {:} tokens.'.format(len(input_ids)))
+
+tokens = tokenizer.convert_ids_to_tokens(input_ids)
+
+for token, id in zip(tokens, input_ids):
+
+    if id == tokenizer.sep_token_id:
+        print('')
+
+    print('{:<12} {:>6,}'.format(token, id))
+
+    if id == tokenizer.sep_token_id:
+        print('')
