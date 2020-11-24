03d47cef8ec075c18f3c853fe0e426c8fc33e5eb
moll_ha <molly.hayward@durham.ac.uk>
2020-11-24 19:01:45 +0000

    started refactor

--- Code/BioELECTRA/pre-training/electra_pytorch-master/pre_training/main.py
+++ Code/BioELECTRA/pre-training/electra_pytorch-master/pre_training/main.py
@@ -5,10 +5,12 @@ from models import ELECTRAModel, get_model_config
 from transformers import ElectraConfig, ElectraTokenizerFast, ElectraForMaskedLM, ElectraForPreTraining
 from hugdatafast import *
 from _utils.utils import *
+import pickle
 
 import os
 from time import time
-from torch import save, load, ones, int64, nn, no_grad
+import torch
+from torch import save, ones, int64, nn, no_grad
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from tqdm import tqdm, trange
 from transformers import MODEL_FOR_QUESTION_ANSWERING_MAPPING, AdamW, get_linear_schedule_with_warmup
@@ -16,27 +18,22 @@ from transformers.data.metrics.squad_metrics import compute_predictions_logits
 from transformers.data.processors.squad import SquadResult
 from torch.utils.tensorboard import SummaryWriter
 
+import datetime
 
-def train(train_dataset, model, tokenizer, model_info, device, settings, dataset_info):
-    """ Train the model """
+now = datetime.now()
+today = datetime.today()
 
-    # specify which directory model checkpoints should be saved to
-    checkpoint_dir = "./checkpoints"
 
-    checkpoint_name = "_epochs6_"
+# Random Sampler used during training.
+data_loader = DataLoader(train_dataset, sampler=RandomSampler(train_dataset), batch_size=settings["batch_size"])
 
-    version_2_with_negative = False
-    overwrite_output_directory = False
-
-    if os.path.exists(checkpoint_dir) and os.listdir(checkpoint_dir) and not overwrite_output_directory:
-        raise ValueError(
-            "Checkpoint directory ({}) already exists. Set overwrite_output_directory to True to overcome.".format(
-                checkpoint_dir
-            )
-        )
-
-    # Random Sampler used during training.
-    data_loader = DataLoader(train_dataset, sampler=RandomSampler(train_dataset), batch_size=settings["batch_size"])
+def create_training_objects(model):
+    """
+    Given whatever parameters necessary - return a new optimizer, tokenizer and scheduler to be used in
+    the training loop
+    :param model:
+    :return: tuple
+    """
 
     # Prepare optimizer and schedule (linear warm up and decay)
     no_decay = ["bias", "LayerNorm.weight"]
@@ -51,38 +48,105 @@ def train(train_dataset, model, tokenizer, model_info, device, settings, dataset
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0,
                                                 num_training_steps=len(data_loader) // settings["epochs"])
 
-    # Check if saved optimizer or scheduler states exist
-    if os.path.isfile(os.path.join(model_info["model_path"], "optimizer.pt")) and os.path.isfile(
-            os.path.join(model_info["model_path"], "scheduler.pt")):
-        # Load in optimizer and scheduler states
-        optimizer.load_state_dict(load(os.path.join(model_info["model_path"], "optimizer.pt")))
-        scheduler.load_state_dict(load(os.path.join(model_info["model_path"], "scheduler.pt")))
+
+    return optimizer, tokenizer, scheduler
+
+
+# Save checkpoint and log every X updates steps.
+def save_model(model, tokenizer, optimizer, scheduler, settings, global_step, tr_loss, save_dir):
+    # set global_step to global_step of last saved checkpoint from model path
+
+    # ------------- SAVE FINE-TUNED TOKENIZER AND MODEL -------------
+    save_dir = os.path.join(save_dir, "checkpoint-{}".format(global_step))
+
+    # Take care of distributed/parallel training
+    saving_model = model.module if hasattr(model, "module") else model
+    saving_model.save_pretrained(save_dir)
+    tokenizer.save_pretrained(save_dir)
+
+    # save training settings with trained model
+    save(settings, os.path.join(save_dir, "train_settings.bin"))
+    save(optimizer.state_dict(), os.path.join(save_dir, "optimizer.pt"))
+    save(scheduler.state_dict(), os.path.join(save_dir, "scheduler.pt"))
+    print("Saving model checkpoint, optimizer and scheduler states to {}".format(save_dir))
+    print("global_step = {}, average loss = {}".format(global_step, tr_loss))
+
+
+
+def update_settings(settings, update):
+    for key, value in update.items():
+        settings[key] = value
+
+    return settings
+
+
+
+def pre_train(data_loader, model, tokenizer, scheduler, optimizer, model_info, device, settings, dataset_info, checkpoint_name=None):
+    """ Train the model """
+    # pass in model, data_loader, optimizer, scheduler and tokenizer
+    # pass in model_name
+    # pass in checkpoint_name
+    settings = {
+        "trainings_epochs": 10,
+        "batch_size": 30,
+        "epochs_trained": 0,
+        "steps_trained": 0,
+        "global_step": 1
+    }
+
+
+    # Specify which directory model checkpoints should be saved to.
+    # Make the checkpoint directory if it does not exist already.
+    checkpoint_dir = "./checkpoints"
+    Path(checkpoint_dir).mkdir(exist_ok=True, parents=True)
+
+    path_to_checkpoint = os.path.join(checkpoint_dir, checkpoint_name)
+    if checkpoint_name and os.path.exists(path_to_checkpoint):
+        # if the directory with the checkpoint name exists, we can retrieve the correct config from here
+        # Load in optimizer, tokenizer and scheduler states
+
+        path_to_optimizer = os.path.join(path_to_checkpoint, "optimizer.pt")
+        if os.path.isfile(path_to_optimizer):
+            optimizer.load_state_dict(torch.load(path_to_optimizer))
+
+        path_to_tokenizer = os.path.join(path_to_checkpoint, "tokenizer.pt")
+        if os.path.isfile(path_to_tokenizer):
+            tokenizer.load_state_dict(torch.load(path_to_tokenizer))
+
+        path_to_scheduler = os.path.join(path_to_checkpoint, "scheduler.pt")
+        if os.path.isfile(path_to_scheduler):
+            scheduler.load_state_dict(torch.load(path_to_scheduler))
+
+        new_settings = torch.load(os.path.join(path_to_checkpoint, "train_settings.bin"))
+        settings = update_settings(settings, new_settings)
+
+        if settings["epochs_trained"] > 0:
+            print("Continuing training from checkpoint, will skip to saved global_step")
+            print("Continuing training from epoch {} and global step {}".format(settings["epochs_trained"], settings["global_step"]))
+
+        if settings["steps_trained"] > 0:
+            print("Skip the first {} steps in the first epoch", settings["steps_trained"])
+
+        # Check if continuing training from a checkpoint
+        if os.path.exists(model_info["model_path"]):
+            pass
+
+
+    # NOW THAT WE HAVE LOADED IN STATES FROM PREVIOUS CHECKPOINT - WE NEED TO CREATE A NEW CHECKPOINT NAME
+    current_day = today.strftime("%d_%m_%y")
+    current_time = now.strftime("%H_%M_%S")
+    checkpoint_name = current_day + '.' + current_time
+
+    version_2_with_negative = False
 
     print("\n---------- BEGIN TRAINING ----------")
     print("Dataset Size = {}\nNumber of Epochs = {}\nBatch size = {}\n"
-          .format(len(train_dataset), settings["epochs"], settings["batch_size"]))
-
-    global_step, epochs_trained = 1, 0
-    steps_trained_in_current_epoch = 0
-
-    # Check if continuing training from a checkpoint
-    if os.path.exists(model_info["model_path"]):
-        try:
-            # set global_step to global_step of last saved checkpoint from model path
-            checkpoint_suffix = model_info["model_path"].split("-")[-1].split("/")[0]
-            global_step = int(checkpoint_suffix)
-            epochs_trained = global_step // (len(data_loader))
-            steps_trained_in_current_epoch = global_step % (len(data_loader))
-
-            print("Continuing training from checkpoint, will skip to saved global_step")
-            print("Continuing training from epoch {} and global step {}".format(epochs_trained, global_step))
-            print("Skip the first {} steps in the first epoch", steps_trained_in_current_epoch)
-        except ValueError:
-            print("Starting fine-tuning.")
+          .format(len(data_loader), settings["training_epochs"], settings["batch_size"]))
+    # todo check that replacing len(dataset) with len(data_loader) is a fair / valid exchange
 
     tr_loss, logging_loss = 0.0, 0.0
     model.zero_grad()
-    train_iterator = trange(epochs_trained, int(settings["epochs"]), desc="Epoch")
+    train_iterator = trange(settings["epochs_trained"], int(settings["epochs"]), desc="Epoch")
 
     # Added here for reproducibility
     # TODO SET SEED HERE AGAIN
@@ -93,8 +157,8 @@ def train(train_dataset, model, tokenizer, model_info, device, settings, dataset
         for step, batch in enumerate(epoch_iterator):
 
             # Skip past any already trained steps if resuming training
-            if steps_trained_in_current_epoch > 0:
-                steps_trained_in_current_epoch -= 1
+            if settings["steps_trained"] > 0:
+                settings["steps_trained"] -= 1
                 continue
 
             # train model one step
@@ -131,32 +195,32 @@ def train(train_dataset, model, tokenizer, model_info, device, settings, dataset
 
             if (step + 1) % 1 == 0:
                 nn.utils.clip_grad_norm_(model.parameters(), settings["max_grad_norm"])
-                global_step += 1
+                settings["global_step"] += 1
 
                 optimizer.step()
                 scheduler.step()  # Update learning rate schedule
                 model.zero_grad()
 
                 # Log metrics
-                if settings["update_steps"] > 0 and global_step % settings["update_steps"] == 0:
+                if settings["update_steps"] > 0 and settings["global_step"] % settings["update_steps"] == 0:
                     # Only evaluate when single GPU otherwise metrics may not average well
                     # Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number"
                     if settings["evaluate_all_checkpoints"]:
                         results = evaluate(model, tokenizer, model_info["model_type"], save_dir, device, settings["evaluate_all_checkpoints"], dataset_info)
                         for key, value in results.items():
-                            tb_writer.add_scalar("eval_{}".format(key), value, global_step)
-                    tb_writer.add_scalar("lr", scheduler.get_lr()[0], global_step)
-                    tb_writer.add_scalar("loss", (tr_loss - logging_loss) / settings["update_steps"], global_step)
+                            tb_writer.add_scalar("eval_{}".format(key), value, settings["global_step"])
+                    tb_writer.add_scalar("lr", scheduler.get_lr()[0], settings["global_step"])
+                    tb_writer.add_scalar("loss", (tr_loss - logging_loss) / settings["update_steps"], settings["global_step"])
                     logging_loss = tr_loss
 
                 # Save model checkpoint
-                if settings["update_steps"] > 0 and global_step % settings["update_steps"] == 0:
-                    save_model(model, tokenizer, optimizer, scheduler, settings, global_step, tr_loss / global_step, save_dir)
+                if settings["update_steps"] > 0 and settings["global_step"] % settings["update_steps"] == 0:
+                    save_model(model, tokenizer, optimizer, scheduler, settings, settings["global_step"], tr_loss / settings["global_step"], save_dir)
 
     tb_writer.close()
 
     # ------------- SAVE FINE-TUNED MODEL -------------
-    save_model(model, tokenizer, optimizer, scheduler, settings, global_step, tr_loss / global_step, save_dir)
+    save_model(model, tokenizer, optimizer, scheduler, settings, settings["global_step"], tr_loss / settings["global_step"], save_dir)
 
 
 
