5700d0e35f7fb4056c4022fd6369335741e5aeee
moll_ha <molly.hayward@durham.ac.uk>
2020-11-24 16:41:39 +0000

    moved example pytorch training loop - about to remove ability to overwrite output dir

--- Code/BioELECTRA/pre-training/electra_pytorch-master/pre_training/main.py
+++ Code/BioELECTRA/pre-training/electra_pytorch-master/pre_training/main.py
@@ -6,6 +6,159 @@ from transformers import ElectraConfig, ElectraTokenizerFast, ElectraForMaskedLM
 from hugdatafast import *
 from _utils.utils import *
 
+import os
+from time import time
+from torch import save, load, ones, int64, nn, no_grad
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from tqdm import tqdm, trange
+from transformers import MODEL_FOR_QUESTION_ANSWERING_MAPPING, AdamW, get_linear_schedule_with_warmup
+from transformers.data.metrics.squad_metrics import compute_predictions_logits
+from transformers.data.processors.squad import SquadResult
+from torch.utils.tensorboard import SummaryWriter
+
+
+def train(train_dataset, model, tokenizer, model_info, device, settings, dataset_info):
+    """ Train the model """
+
+    # specify which directory model checkpoints should be saved to
+    checkpoint_dir = "./checkpoints"
+
+    checkpoint_name = "_epochs6_"
+
+    version_2_with_negative = False
+    overwrite_output_directory = False
+
+    if os.path.exists(checkpoint_dir) and os.listdir(checkpoint_dir) and not overwrite_output_directory:
+        raise ValueError(
+            "Checkpoint directory ({}) already exists. Set overwrite_output_directory to True to overcome.".format(
+                checkpoint_dir
+            )
+        )
+
+    # Random Sampler used during training.
+    data_loader = DataLoader(train_dataset, sampler=RandomSampler(train_dataset), batch_size=settings["batch_size"])
+
+    # Prepare optimizer and schedule (linear warm up and decay)
+    no_decay = ["bias", "LayerNorm.weight"]
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+            "weight_decay": settings["decay"],
+        },
+        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
+    ]
+    optimizer = AdamW(optimizer_grouped_parameters, eps=settings["epsilon"], lr=settings["learning_rate"])
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0,
+                                                num_training_steps=len(data_loader) // settings["epochs"])
+
+    # Check if saved optimizer or scheduler states exist
+    if os.path.isfile(os.path.join(model_info["model_path"], "optimizer.pt")) and os.path.isfile(
+            os.path.join(model_info["model_path"], "scheduler.pt")):
+        # Load in optimizer and scheduler states
+        optimizer.load_state_dict(load(os.path.join(model_info["model_path"], "optimizer.pt")))
+        scheduler.load_state_dict(load(os.path.join(model_info["model_path"], "scheduler.pt")))
+
+    print("\n---------- BEGIN TRAINING ----------")
+    print("Dataset Size = {}\nNumber of Epochs = {}\nBatch size = {}\n"
+          .format(len(train_dataset), settings["epochs"], settings["batch_size"]))
+
+    global_step, epochs_trained = 1, 0
+    steps_trained_in_current_epoch = 0
+
+    # Check if continuing training from a checkpoint
+    if os.path.exists(model_info["model_path"]):
+        try:
+            # set global_step to global_step of last saved checkpoint from model path
+            checkpoint_suffix = model_info["model_path"].split("-")[-1].split("/")[0]
+            global_step = int(checkpoint_suffix)
+            epochs_trained = global_step // (len(data_loader))
+            steps_trained_in_current_epoch = global_step % (len(data_loader))
+
+            print("Continuing training from checkpoint, will skip to saved global_step")
+            print("Continuing training from epoch {} and global step {}".format(epochs_trained, global_step))
+            print("Skip the first {} steps in the first epoch", steps_trained_in_current_epoch)
+        except ValueError:
+            print("Starting fine-tuning.")
+
+    tr_loss, logging_loss = 0.0, 0.0
+    model.zero_grad()
+    train_iterator = trange(epochs_trained, int(settings["epochs"]), desc="Epoch")
+
+    # Added here for reproducibility
+    # TODO SET SEED HERE AGAIN
+
+    tb_writer = SummaryWriter()  # Create a SummaryWriter()
+    for _ in train_iterator:
+        epoch_iterator = tqdm(data_loader, desc="Iteration")
+        for step, batch in enumerate(epoch_iterator):
+
+            # Skip past any already trained steps if resuming training
+            if steps_trained_in_current_epoch > 0:
+                steps_trained_in_current_epoch -= 1
+                continue
+
+            # train model one step
+            model.train()
+            batch = tuple(t.to(device) for t in batch)
+
+            inputs = {
+                "input_ids": batch[0],
+                "attention_mask": batch[1],
+                "token_type_ids": batch[2],
+                "start_positions": batch[3],
+                "end_positions": batch[4],
+            }
+
+            if model_info["model_type"] in ["xlm", "roberta", "distilbert", "camembert"]:
+                del inputs["token_type_ids"]
+
+            if model_info["model_type"] in ["xlnet", "xlm"]:
+                inputs.update({"cls_index": batch[5], "p_mask": batch[6]})
+                if version_2_with_negative:
+                    inputs.update({"is_impossible": batch[7]})
+                if hasattr(model, "config") and hasattr(model.config, "lang2id"):
+                    inputs.update(
+                        {"langs": (ones(batch[0].shape, dtype=int64) * 0).to(device)}
+                    )
+
+            outputs = model(**inputs)
+
+            # model outputs are always tuple in transformers (see doc)
+            loss = outputs[0]
+            loss.backward()
+
+            tr_loss += loss.item()
+
+            if (step + 1) % 1 == 0:
+                nn.utils.clip_grad_norm_(model.parameters(), settings["max_grad_norm"])
+                global_step += 1
+
+                optimizer.step()
+                scheduler.step()  # Update learning rate schedule
+                model.zero_grad()
+
+                # Log metrics
+                if settings["update_steps"] > 0 and global_step % settings["update_steps"] == 0:
+                    # Only evaluate when single GPU otherwise metrics may not average well
+                    # Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number"
+                    if settings["evaluate_all_checkpoints"]:
+                        results = evaluate(model, tokenizer, model_info["model_type"], save_dir, device, settings["evaluate_all_checkpoints"], dataset_info)
+                        for key, value in results.items():
+                            tb_writer.add_scalar("eval_{}".format(key), value, global_step)
+                    tb_writer.add_scalar("lr", scheduler.get_lr()[0], global_step)
+                    tb_writer.add_scalar("loss", (tr_loss - logging_loss) / settings["update_steps"], global_step)
+                    logging_loss = tr_loss
+
+                # Save model checkpoint
+                if settings["update_steps"] > 0 and global_step % settings["update_steps"] == 0:
+                    save_model(model, tokenizer, optimizer, scheduler, settings, global_step, tr_loss / global_step, save_dir)
+
+    tb_writer.close()
+
+    # ------------- SAVE FINE-TUNED MODEL -------------
+    save_model(model, tokenizer, optimizer, scheduler, settings, global_step, tr_loss / global_step, save_dir)
+
+
 
 if __name__ == "__main__":
 
