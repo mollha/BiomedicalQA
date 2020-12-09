d9d9618f1ae332a88b007fd4a9bfd00dc41712f8
moll_ha <molly.hayward@durham.ac.uk>
2020-12-09 14:54:32 +0000

    cleaned code again for generating vocab

--- Code/pre_training/train_tokenizer.py
+++ Code/pre_training/train_tokenizer.py
@@ -25,7 +25,7 @@ def find_text_files(directory):
     return [str(file) for file in list(directory.glob('*.csv'))]
 
 
-def train_bio_tokenizer():
+def train_bio_tokenizer(path_to_output: str):
     processed_data_directory = (base_path / '../datasets/PubMed/processed_data').resolve()
     text_files = find_text_files(processed_data_directory)
 
@@ -46,12 +46,15 @@ def train_bio_tokenizer():
     )
 
     bio_tokenizer.train(trainer, text_files)
-    bio_tokenizer.model.save("bio_tokenizer", "electra-pubmed")
+    bio_tokenizer.model.save(path_to_output, "electra-pubmed")
 
 
-def create_tokenizers(additional_vocab_path):
+def create_tokenizers(additional_vocab_path, path_to_output: str):
     file = open(additional_vocab_path)
     lines = file.readlines()
+
+    for idx in range(len(lines)):
+        lines[idx] = lines[idx].strip()
 
     sizes = ["small", "base", "large"]
 
@@ -59,26 +62,22 @@ def create_tokenizers(additional_vocab_path):
         print("\nExtending the vocabulary of the pre-trained {} tokenizer to include words from the PubMed corpus."
               .format(f'google/electra-{size}-generator'))
         electra_tokenizer = ElectraTokenizerFast.from_pretrained(f'google/electra-{size}-generator')
-        for line in lines:
-            electra_tokenizer.add_tokens(line)
-
-        model_files = electra_tokenizer.model.save("bio_tokenizer", "electra-duo-corpus")
-        electra_tokenizer.model = WordPiece.from_file(*model_files, unk_token="[UNK]")
-        electra_tokenizer.save("bio_tokenizer/electra-duo-corpus.json")
-        electra_tokenizer.save_pretrained('bio_tokenizer/bio_electra_{}_tokenizer'.format(size))
+        electra_tokenizer.add_tokens(lines)
+        electra_tokenizer.save_pretrained(path_to_output + '/bio_electra_{}_tokenizer'.format(size))
 
     file.close()
 
 
 if __name__ == "__main__":
-    path_to_vocab = (base_path / "bio_tokenizer/electra-pubmed-vocab.txt").resolve()
     path_to_output_dir = (base_path / "bio_tokenizer").resolve()
     pathlib.Path(path_to_output_dir).mkdir(exist_ok=True, parents=True)
+
+    path_to_vocab = (path_to_output_dir / "electra-pubmed-vocab.txt").resolve()
     vocab_exists = path_to_vocab.exists()
 
     print("Path to {} {}, {} vocab.".format(str(path_to_vocab), "exists" if vocab_exists else "does not exist",
                                             "re-using" if vocab_exists else "creating"))
     if not vocab_exists:
-        train_bio_tokenizer()
+        train_bio_tokenizer(str(path_to_output_dir))
 
-    create_tokenizers(path_to_vocab)
+    create_tokenizers(path_to_vocab, str(path_to_output_dir))
