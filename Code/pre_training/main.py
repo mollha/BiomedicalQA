562c9b01922c815ce81a5e552766d3c212addbd9
moll_ha <molly.hayward@durham.ac.uk>
2020-12-17 21:44:11 +0000

    reduce batch size to tackle memory error

--- Code/pre_training/main.py
+++ Code/pre_training/main.py
@@ -19,7 +19,7 @@ config = {
     'adam_bias_correction': False,
     'generator_loss': [],
     'discriminator_loss': [],
-    'batch_size': 128,  # override batch size as not enough memory
+    'batch_size': 32,  # override batch size as not enough memory
     'size': 'base',  # electra small too small for QA
     'num_workers': 3 if torch.cuda.is_available() else 0,
     "max_epochs": 9999,
