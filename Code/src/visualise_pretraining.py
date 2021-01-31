6a00ea42c1f68b76ceb20f774db616b45eab2962
moll_ha <molly.hayward@durham.ac.uk>
2021-01-31 14:14:04 +0000

    Creating subplots of pre-training stats

--- Code/src/visualise_pretraining.py
+++ Code/src/visualise_pretraining.py
@@ -6,12 +6,13 @@ import pickle
 
 base_path = Path(__file__).parent
 checkpoint_dir = (base_path / '../checkpoints/pretrain').resolve()
-graphs_path = (base_path / 'pre_training_graphs').resolve()
+graphs_path = (base_path / 'visualisations/pre_training_graphs').resolve()
 Path(graphs_path).mkdir(exist_ok=True, parents=True)
 
 
-def draw_graph(graph_title, data, data_label, epochs, checkpoint_name, y_label=None, more_data=None, more_data_label=None):
-    plt.plot(epochs, data, label=data_label)
+def draw_graph(graph_title, data, data_label, epochs, checkpoint_name, y_label=None, more_data=None,
+               more_data_label=None, save_figure=True, color="b"):
+    plt.plot(epochs, data, label=data_label, color=color)
     if more_data is not None:
         plt.plot(epochs, more_data, label=more_data_label)
 
@@ -22,8 +23,11 @@ def draw_graph(graph_title, data, data_label, epochs, checkpoint_name, y_label=N
 
     plt.ylabel(y_label)
     plt.legend()
-    plt.savefig((graphs_path / graph_save_name).resolve())
-    plt.show()
+
+    if save_figure:
+        plt.savefig((graphs_path / graph_save_name).resolve())  # this is saving them weird due to subplots
+        plt.show()
+    return plt
 
 
 def load_stats_from_checkpoint(path_to_checkpoint, checkpoint_name):
@@ -37,7 +41,6 @@ def load_stats_from_checkpoint(path_to_checkpoint, checkpoint_name):
 
     combined_losses, generator_losses, discriminator_losses, discriminator_accuracy, \
     discriminator_precision, discriminator_recall = loss_function.get_statistics()
-    num_epochs = range(1, len(generator_losses) + 1)
 
     print("Combined Losses:", combined_losses)
     print("Generator Losses:", generator_losses)
@@ -45,9 +48,87 @@ def load_stats_from_checkpoint(path_to_checkpoint, checkpoint_name):
     print("Discriminator Accuracy:", discriminator_accuracy)
     print("Discriminator Precision:", discriminator_precision)
     print("Discriminator Recall:", discriminator_recall)
+    return combined_losses, generator_losses, discriminator_losses, discriminator_accuracy, discriminator_precision, \
+           discriminator_recall
 
+
+def create_subplots(statistics, checkpoint_name):
+    combined_losses, generator_losses, discriminator_losses, discriminator_accuracy, discriminator_precision, \
+    discriminator_recall = statistics
+
+    num_epochs = range(1, len(generator_losses) + 1)
+    fig = plt.figure(0, figsize=(8, 8))
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    # fig.suptitle("Statistics during Pre-training", fontsize=12, fontweight="bold", y=0.96)
+
+    # Note: we don't include this in the subplots for now. It can be a standalone graph.
+    # print("Creating graph of Generator and Discriminator Loss")
+    # draw_graph(graph_title="Generator and Discriminator Loss during pre-training",
+    #                                      data=generator_losses,
+    #                                      data_label="Generator loss",
+    #                                      epochs=num_epochs,
+    #                                      checkpoint_name=checkpoint_name,
+    #                                      y_label="Loss",
+    #                                      more_data=discriminator_losses,
+    #                                      more_data_label="Discriminator loss")
+
+    plt.subplot(2, 2, 1)
+    print("\nCreating graph of Combined Loss")
+    draw_graph(graph_title="Combined Loss",
+               data=combined_losses,
+               data_label="Loss",
+               epochs=num_epochs,
+               checkpoint_name=checkpoint_name,
+               color="b",
+               save_figure=False)
+
+
+    print("Creating graph of Discriminator Accuracy")
+    plt.subplot(2, 2, 2)
+    draw_graph(graph_title="Discriminator Accuracy",
+               data=discriminator_accuracy,
+               data_label="Discriminator Accuracy",
+               epochs=num_epochs,
+               checkpoint_name=checkpoint_name,
+               color="g",
+               save_figure=False)
+
+
+    plt.subplot(2, 2, 3)
+    print("Creating graph of Discriminator Precision")
+    draw_graph(graph_title="Discriminator Precision",
+               data=discriminator_precision,
+               data_label="Discriminator Precision",
+               epochs=num_epochs,
+               checkpoint_name=checkpoint_name,
+               color="r",
+               save_figure=False)
+
+    plt.subplot(2, 2, 4)
+    print("Creating graph of Discriminator Recall")
+    draw_graph(graph_title="Discriminator Recall",
+               data=discriminator_recall,
+               data_label="Discriminator Recall",
+               epochs=num_epochs,
+               checkpoint_name=checkpoint_name,
+               color="c",
+               save_figure=False)
+
+
+    plt.subplots_adjust(hspace=0.35, wspace=0.4)
+    plt.show()
+    print("\nGraph creation complete.\n")
+
+
+def create_separate_plots(statistics, checkpoint_name):
+    combined_losses, generator_losses, discriminator_losses, discriminator_accuracy, discriminator_precision, \
+    discriminator_recall = statistics
+
+    fig_size = (5, 5)
+    num_epochs = range(1, len(generator_losses) + 1)
 
     print("Creating graph of Generator and Discriminator Loss")
+    plt.figure(1, figsize=fig_size)
     draw_graph(graph_title="Generator and Discriminator Loss during pre-training",
                data=generator_losses,
                data_label="Generator loss",
@@ -57,42 +138,47 @@ def load_stats_from_checkpoint(path_to_checkpoint, checkpoint_name):
                more_data=discriminator_losses,
                more_data_label="Discriminator loss")
 
-    print("Creating graph of Combined Loss")
-    draw_graph(graph_title="Loss during pre-training",
+    print("\nCreating graph of Combined Loss")
+    plt.figure(2, figsize=fig_size)
+    draw_graph(graph_title="Loss during Pre-training",
                data=combined_losses,
                data_label="Loss",
                epochs=num_epochs,
                checkpoint_name=checkpoint_name)
 
     print("Creating graph of Discriminator Accuracy")
-    draw_graph(graph_title="Discriminator Accuracy during pre-training",
+    plt.figure(3, figsize=fig_size)
+    draw_graph(graph_title="Discriminator Accuracy during Pre-training",
                data=discriminator_accuracy,
                data_label="Discriminator Accuracy",
                epochs=num_epochs,
                checkpoint_name=checkpoint_name)
 
     print("Creating graph of Discriminator Precision")
-    draw_graph(graph_title="Discriminator Precision during pre-training",
+    plt.figure(4, figsize=fig_size)
+    draw_graph(graph_title="Discriminator Precision during Pre-training",
                data=discriminator_precision,
                data_label="Discriminator Precision",
                epochs=num_epochs,
                checkpoint_name=checkpoint_name)
 
     print("Creating graph of Discriminator Recall")
+    plt.figure(5, figsize=fig_size)
     draw_graph(graph_title="Discriminator Recall during pre-training",
                data=discriminator_recall,
                data_label="Discriminator Recall",
                epochs=num_epochs,
                checkpoint_name=checkpoint_name)
-
-    print("Graph creation complete.\n")
+    print("\nGraph creation complete.\n")
 
 
 if __name__ == "__main__":
-    chckpt_name = "small_22_87883"    # e.g. small_10_50
+    chckpt_name = "small_26_11229"  # e.g. small_10_50
 
     if len(chckpt_name) == 0:
         raise ValueError("Checkpoint name must be the name of a valid checkpoint e.g. small_10_50")
 
     checkpoint_path = (checkpoint_dir / chckpt_name).resolve()
-    load_stats_from_checkpoint(checkpoint_path, chckpt_name)
+    stats = load_stats_from_checkpoint(checkpoint_path, chckpt_name)
+    create_subplots(stats, chckpt_name)
+    create_separate_plots(stats, chckpt_name)
