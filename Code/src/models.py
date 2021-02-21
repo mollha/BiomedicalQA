2320b1ea025d28283fc43861dae37996c5a122c3
moll_ha <molly.hayward@durham.ac.uk>
2021-02-21 22:54:37 +0000

    testing

--- Code/src/models.py
+++ Code/src/models.py
@@ -439,7 +439,10 @@ class CostSensitiveSequenceClassification(ElectraForSequenceClassification):
                 if weights is not None:
                     loss_fct = BCELoss(weight=weights)
                     print("used the weighted loss fc")
-                loss = loss_fct(logits.view(-1), labels.view(-1))
+                print('logits', logits.view(-1, self.num_labels))
+                print('labels', labels.view(-1))
+
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
 
                 # loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
 
