7fb5b708dbc0bd987ae4a9aeb42339fd4f7c7619
moll_ha <molly.hayward@durham.ac.uk>
2021-02-21 21:32:14 +0000

    switched datasets back and reduced lr

--- Code/src/data_processing.py
+++ Code/src/data_processing.py
@@ -14,9 +14,9 @@ import random
 # ----------------------- SPECIFY DATASET PATHS -----------------------
 # the folder structure of bioasq is different to squad, as we need to download matching articles
 datasets = {
-    "bioasq": {"train": ["raw_data/8B1_golden.json", "raw_data/8B2_golden.json", "raw_data/8B3_golden.json",
+    "bioasq": {"train": ["raw_data/training9b.json"],
+               "test": ["raw_data/8B1_golden.json", "raw_data/8B2_golden.json", "raw_data/8B3_golden.json",
                         "raw_data/8B4_golden.json", "raw_data/8B5_golden.json"],
-               "test": ["raw_data/training9b.json"]
                },
     "squad": {
         "train": ["train-v2.0.json"],
