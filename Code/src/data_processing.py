2d8ebeb8d941883b1f3451ced5b1f0ea5e9e1100
moll_ha <molly.hayward@durham.ac.uk>
2021-02-28 18:54:42 +0000

    getting training stats for 8b

--- Code/src/data_processing.py
+++ Code/src/data_processing.py
@@ -14,12 +14,12 @@ import random
 # ----------------------- SPECIFY DATASET PATHS -----------------------
 # the folder structure of bioasq is different to squad, as we need to download matching articles
 datasets = {
-    "bioasq": {"train": ["raw_data/training9b.json"],
-               "test": ["raw_data/8B1_golden.json", "raw_data/8B2_golden.json", "raw_data/8B3_golden.json",
-                        "raw_data/8B4_golden.json", "raw_data/8B5_golden.json"],
-               },
-               #  "test": ["raw_data/8B_golden.json"],
+    "bioasq": {"train": ["raw_data/training8b.json"],
+               # "test": ["raw_data/8B1_golden.json", "raw_data/8B2_golden.json", "raw_data/8B3_golden.json",
+               #          "raw_data/8B4_golden.json", "raw_data/8B5_golden.json"],
                # },
+                "test": ["raw_data/8B_golden.json"],
+               },
     "boolq": {"train": ["combined.jsonl"],
                "test": ["my_test.jsonl"],
                },
