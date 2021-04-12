92fe344a4fdfdbcfa7c85a260031b3075c01cf74
moll_ha <molly.hayward@durham.ac.uk>
2021-04-12 16:27:09 +0100

    small update steps

--- Code/src/fine_tuning.py
+++ Code/src/fine_tuning.py
@@ -22,7 +22,7 @@ config = {
     "current_epoch": 0,  # track the current epoch in config for saving checkpoints
     "steps_trained": 0,  # track the steps trained in config for saving checkpoints
     "global_step": -1,  # total steps over all epochs
-    "update_steps": 2000,  # set this really high for now.
+    "update_steps": 200,  # set this really high for now.
     "pretrained_settings": {
         "epochs": 0,
         "steps": 0,
